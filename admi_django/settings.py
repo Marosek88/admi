06f49482fe529c4f8a2a2c969ff8eb867ccd8e60
Ubuntu <ubuntu@ip-172-31-20-87.eu-west-1.compute.internal>
2019-07-18 20:14:21 +0000

    before heroku

--- admi_django/settings.py
+++ admi_django/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    # 'crispy_forms',
-    # 'storages',
+    'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
