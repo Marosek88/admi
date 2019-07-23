67180e2310665a2914387df72c41209a5dd7a3a5
Ubuntu <ubuntu@ip-172-31-20-87.eu-west-1.compute.internal>
2019-07-23 11:53:37 +0000

    before localising

--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     
     return render(request, 'website/index.html')
+    
+def pl(request):
+    
+    return render(request, 'website/index.html')
